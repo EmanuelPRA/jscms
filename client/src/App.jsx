90d73c11e44666f2e80fa193261476a72bda7eec
Emanuel Pracic <epracic@tvz.hr>
2025-11-28 06:31:28 +0100

    SCSS conversion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
 
